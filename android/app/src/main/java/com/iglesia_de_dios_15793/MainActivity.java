13fa2a3b62a4cabe4a768a6c2433fe71bb4993cf
Crowdbotics <team@crowdbotics.com>
2020-04-13 07:40:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/iglesia_de_dios_15793/MainActivity.java
@@ -0,0 +1,15 @@
+package com.iglesia_de_dios_15793;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "iglesia_de_dios_15793";
+    }
+}
